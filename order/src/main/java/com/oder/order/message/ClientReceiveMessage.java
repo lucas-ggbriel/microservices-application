9b029510b33f3e9ddb49f483dd6efe15aeca6a30
Lucas Gabriel <lucas.gato.03092001@gmail.com>
2021-06-30 00:09:33 -0300

    Fix bug on consumer; Consuming menu messages

--- order/src/main/java/com/oder/order/message/ClientReceiveMessage.java
+++ order/src/main/java/com/oder/order/message/ClientReceiveMessage.java
@@ -1,11 +1,15 @@
 package com.oder.order.message;
 
 import com.oder.order.dto.ClientOrderDto;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ClientReceiveMessage {
 
+    @RabbitListener(queues = {"${register.client.rabbitmq.queue}"})
     public void receiveMessage(@Payload ClientOrderDto clientOrderDto){
-
+        System.out.println(clientOrderDto);
     }
 }
